4583ef645feafe9f034c7168b80a8708aa46213d
Magnum888 <K.O.M.888@i.ua>
2018-04-03 19:59:27 +0300

    add webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
     src: path.join(__dirname, 'src'),
     dist: path.join(__dirname, 'dist')
-}
+};
 
 const common = {
   entry: PATHS.src + '/app.js',
@@ -21,28 +23,24 @@ const common = {
 };
 const productionConfig = {
   mode: 'production'
-}
+};
 const developmentConfig = {
-  devServer: {
-    stats: 'errors-only',
-    port: 9000
-  },
   mode: 'development'
 };
 
+
 module.exports = function(env){
   if(env === 'production'){
-    return Object.assign(
-      {},
+    return merge([
       common,
       productionConfig
-    )
+    ])
   }
   if(env === 'development'){
-    return Object.assign(
-      {},
+    return merge([
       common,
-      developmentConfig
-    )
+      developmentConfig,
+      devserver()
+    ])
   }
-}
+};
